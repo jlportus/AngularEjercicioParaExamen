fa32b53dbf53977637edc6516eb548d0c5a2ee6b
JLPP <jl_puerto@hotmail.com>
2021-04-27 21:53:48 +0200

    Creado Proyecto-Incluida Libreria Bootstrap

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  title: `Pagina Web de Preparacion para el Examen`,
 };
 
 /*
