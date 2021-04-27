51a8733ccb496a9047c182877e24af2547e1ae31
JLPP <jl_puerto@hotmail.com>
2021-04-27 22:30:20 +0200

    Creado el Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./comunes/home/home/home.component";
 import { EsqueletoComponent } from "./esqueleto/esqueleto.component";
 import { NoEncontradoComponent } from "./esqueleto/no-encontrado/no-encontrado.component";
 
 const routes: Routes = [
-  // {
-  //   path: ``,
-  //   component: EsqueletoComponent,
-  // },
+  {
+    path: ``,
+    component: HomeComponent,
+  },
   {
     path: `**`,
     redirectTo: `not-found`,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HttpClient } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { EsqueletoModule } from "./esqueleto/esqueleto.module";
+import { HomeModule } from "./comunes/home/home.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, EsqueletoModule],
+  imports: [BrowserModule, AppRoutingModule, EsqueletoModule, HomeModule, HttpClientModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
