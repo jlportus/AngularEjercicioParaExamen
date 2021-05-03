612697361f21bfb2ae5815a7557aec04038cc53c
JLPP <jl_puerto@hotmail.com>
2021-05-03 14:22:50 +0200

    Refactor partido xa mostrar el recibido del padre

--- src/app/ejercicios/partidos/partidos/partido/partido.component.ts
+++ src/app/ejercicios/partidos/partidos/partido/partido.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Partido } from "../../moduls/partido";
+import { Suceso } from "../../moduls/suceso";
 
 @Component({
   selector: "app-partido",
@@ -8,9 +9,7 @@ import { Partido } from "../../moduls/partido";
 })
 export class PartidoComponent implements OnInit {
   @Input() partido: Partido;
-  public idLocal: string = ``;
-  public idVisitante: string = ``;
-  public competicion: string = ``;
+
   public resultado: string = ``;
   public visibilidadTarjetas: boolean;
   public tarjetas: string = ``;
@@ -20,16 +19,39 @@ export class PartidoComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    console.log(this.partido)
-    this.idLocal = `local`;
-    this.idVisitante = `visitante`;
-    this.competicion = `laLiga`;
-    this.resultado = `0-0`;
+    console.log(this.partido);
+    this.resultado = this.calcularResultado();
+    console.log(this.resultado);
+
     this.tarjetas = `8 tarjetas`;
   }
+
   public mostrarTarjetas(): void {
     this.visibilidadTarjetas = !this.visibilidadTarjetas;
     console.log("esztas en el evento");
     console.log(this.estado);
+  }
+
+  calcularResultado(): string {
+    return (
+      this.calcularEventosEquipo(this.partido.idVisitante, "Gol") +
+      "-" +
+      this.calcularEventosEquipo(this.partido.idLocal, "Gol")
+    );
+  }
+
+  calcularEventosEquipo(idEquipo: string, evento: string): number {
+    let numeroVecesEvento = 0;
+    this.partido.sucesos.forEach((s: Suceso) => {
+      console.log(s);
+      console.log(s.idParticipante);
+      if (s.class == evento && s.idParticipante == idEquipo) {
+        console.log(true);
+        numeroVecesEvento++;
+      } else {
+        console.log(false);
+      }
+    });
+    return numeroVecesEvento;
   }
 }
